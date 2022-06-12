48f6426fbc60281b4936becb07a934e4c43cb766
Nishant-l <nishant3151998@gmail.com>
2022-06-12 10:52:32 +0530

    Added message to add to cart when no item is added for checkout

--- src/Components/ShowTotal.js
+++ src/Components/ShowTotal.js
@@ -4,17 +4,23 @@ import { Divider } from '@mui/material';
 export const ShowTotal = ({total}) => {
     return(
         <article className="message is-danger">
-            <div className="message-header" style={{justifyContent:'center'}}>
+            {total !== 0 && <div className="message-header" style={{justifyContent:'center'}}>
                 <p>Total Amount</p>
-            </div>
+            </div>}
             <div className="message-body" style={{textAlign:'justify'}}>
+            {total!==0 ?
+                <>
                 <div>Total MRP: {total} ₹</div>
                 <div> {`Discount   :0 ₹`}</div>
                 <Divider sx={{margin:'5px', color:'black'}}/>
                 <div><strong>Total: {total} ₹</strong></div>
                 <div style={{marginTop:'10px' }}><button class="button is-success" style={{width:'100%'}}>Pay</button></div>
+                </>
+                :
+                <>
+                    Your CART is Empty, Please Add Items...🚚...🚚...🚚
+                </>}
             </div>
         </article>
-
     )
 }
